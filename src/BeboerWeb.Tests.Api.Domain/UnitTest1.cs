f76a689682a0f2b0eea60060b96c74e7795bcef5
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2021-11-07 20:43:24 +0100

    Added more projects

---
+++ src/BeboerWeb.Tests.Api.Domain/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BeboerWeb.Tests.Api.Domain
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
