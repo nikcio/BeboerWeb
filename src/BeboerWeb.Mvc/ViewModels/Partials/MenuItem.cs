c38713cb0724f7bb73e6d4b08d996c72ab439443
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2021-11-10 09:20:37 +0100

    Added Menu item

---
+++ src/BeboerWeb.Mvc/ViewModels/Partials/MenuItem.cs
@@ -0,0 +1,9 @@
+﻿namespace BeboerWeb.Mvc.ViewModels.Partials
+{
+    public class MenuItem
+    {
+        public string Link { get; set; }
+        public string MenuTitle { get; set; }
+        public string Icon { get; set; }
+    }
+}
