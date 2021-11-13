32637265dfa69a22ed15d35e05e359d570232f53
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2021-11-13 11:03:41 +0100

    Added Booking Items

---
+++ src/BeboerWeb.Api.Domain/Models/Bookings/BookingWindow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeboerWeb.Api.Domain.Models.Bookings
+{
+    public class BookingWindow
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public virtual Calender Calender { get; set; }
+        public virtual IEnumerable<Booking> Bookings { get; set; }
+    }
+}
---
+++ src/BeboerWeb.Api.Domain/Models/Bookings/IBookingItem.cs
@@ -0,0 +1,7 @@
+﻿namespace BeboerWeb.Api.Domain.Models.Bookings
+{
+    public interface IBookingItem
+    {
+        int Id { get; set; }
+    }
+}
