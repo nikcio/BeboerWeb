0f4895060105a52c4fd3bd6b3fbd20fc8d54f958
andersen965 <74706368+andersen965@users.noreply.github.com>
2021-11-15 18:16:27 +0100

    Create DocumentLease view

--- src/BeboerWeb.Mvc/Controllers/HomeController.cs
+++ src/BeboerWeb.Mvc/Controllers/HomeController.cs
@@ -32,6 +32,10 @@ namespace BeboerWeb.Mvc.Controllers
         {
             return View();
         }
+        public IActionResult DocumentLease()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
