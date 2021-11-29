6b5521882554ddd87f19f28510050f5896321061
Gustav <mingvst@gmail.com>
2021-11-29 11:34:37 +0100

    mere af det dersens backend du ved
    I hope Zoe wins xD. I’m a Zoe main and she’s just so fun!! People get so trolled by the bubble, and her voice lines are so cute like when she sings about chocolate cake LOL! She’s super random but also smarter than she looks, just like me xD

--- src/BeboerWeb.Api.Persistence/Repositories/Chat/TenantToEmployeeMessageRepository.cs
+++ src/BeboerWeb.Api.Persistence/Repositories/Chat/TenantToEmployeeMessageRepository.cs
@@ -3,6 +3,8 @@ using BeboerWeb.Api.Domain.Models.Chat;
 using BeboerWeb.Api.Persistence.Contexts;
 using BeboerWeb.Api.Persistence.Repositories.Bases;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace BeboerWeb.Api.Persistence.Repositories.Chat
 {
@@ -10,6 +12,25 @@ namespace BeboerWeb.Api.Persistence.Repositories.Chat
     {
         public TenantToEmployeeMessageRepository(IApiDbContext dbContext, ILogger<CrudRepositoryBase<TenantToEmployeeMessage>> logger) : base(dbContext, logger)
         {
+        }
+
+        public override async Task<IEnumerable<TenantToEmployeeMessage>> GetAllAsync()
+        {
+            var output = await base.GetAllAsync();
+            foreach (var item in output)
+            {
+                await GetDBContext().Entry(item).Collection(p => p.Receivers).LoadAsync();
+                await GetDBContext().Entry(item).Reference(p => p.Sender).LoadAsync();
+            }
+            return output;
+        }
+
+        public async override Task<TenantToEmployeeMessage> GetByIdAsync(int id)
+        {
+            var output = await base.GetByIdAsync(id);
+            await GetDBContext().Entry(output).Collection(p => p.Receivers).LoadAsync();
+            await GetDBContext().Entry(output).Reference(p => p.Sender).LoadAsync();
+            return output;
         }
     }
 }
